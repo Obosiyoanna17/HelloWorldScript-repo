36fb08dab4f3df45403527ff14408ce99ba68d1a
Obosiyoanna17 <joannaoboh@gmail.com>
2024-07-04 20:33:40 +0100

    project with npm init

---
+++ project.js
@@ -0,0 +1,9 @@
+//  Simple project using npm init
+var myDate = new Date();
+var months = ["january", "February", "March", "April", "May", "June", "July", "August", "September", "october"...]
+var month = myDate.getMonth();
+var day = myDate.getDay();
+
+console.log(myDate);
+console.log(months[month]);
+console.log(day);globalThis
